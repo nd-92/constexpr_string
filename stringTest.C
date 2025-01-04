c313fbe1c8023c1d616caa04dbf625168975b074
nd92 <nd203@leicester.ac.uk>
2025-01-04 10:52:13 +0000

    Implement cout << operator

--- stringTest.C
+++ stringTest.C
@@ -1,19 +1,37 @@
 #include <iostream>
 #include "constexpr_string.H"
 
+constexpr const std::integral_constant<uint64_t, 1434525> i_pos;
+constexpr const std::integral_constant<uint64_t, 0> i_0;
+constexpr const std::integral_constant<int64_t, -12341> j_neg;
+constexpr const std::integral_constant<int64_t, 0> j_0;
+constexpr const std::integral_constant<int64_t, 423341> j_pos;
+constexpr const constexpr_string buffer =
+    constexpr_string("This is the generated test string.") //
+    + "\n"                                                 //
+    + "The converted integral constants are: "             //
+    + i_0 + ", "                                           //
+    + i_pos + ", "                                         //
+    + j_neg + ", "                                         //
+    + j_0 + ", "                                           //
+    + j_pos + "\n"                                         //
+    + "This string compiling successfully tests both the addition operator and the conversion of integral_constants to constexpr_string objects.";
+
 int main()
 {
     // constexpr const std::integral_constant<unsigned int, 124701383> i;
     // static constexpr const constexpr_string buffer = constexpr_string(integral_to_char_array(std::integral_constant<uint64_t, 124701383>()));
     // static constexpr const constexpr_string buffer = constexpr_string(std::integral_constant<uint64_t, 124701383>());
 
-    constexpr const std::integral_constant<uint64_t, 1434525> i;
+    // std::cout << buffer.string_view() << std::endl;
 
-    std::cout << i() << " has string length " << constexpr_strlen(i) << std::endl;
+    std::cout << buffer << std::endl;
 
-    static constexpr const constexpr_string buffer = constexpr_string("Buffer: ") + i + constexpr_string(", which seems correct");
+    // std::cout << i() << " has string length " << constexpr_strlen(i) << std::endl;
 
-    std::cout << buffer.string_view() << std::endl;
+    // static constexpr const constexpr_string buffer = constexpr_string("Buffer: ") + i + constexpr_string(", which seems correct");
+
+    // std::cout << buffer.string_view() << std::endl;
 
     // std::cout << "Buffer:" << std::endl;
     // for (std::size_t k = 0; k < buffer.size() - 1; k++)
